4a68dfe642a34f9e7750b7761b946b74829590b7
xuchao <85825770@qq.com>
2017-01-25 18:08:39 +0800

    cc

--- src/views/note/view.php
+++ src/views/note/view.php
@@ -1,4 +1,16 @@
 <?php
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use xutl\highlightjs\HighlightJsAsset;
+
+HighlightJsAsset::register($this);
+$this->registerJs('
+    $(\'pre code\').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
+');
+
+
 ?>
 <?=$model->title;?>
-<?=$model->content;?>
+<pre class="<?=$model->format?>"><?=Html::encode($model->content);?></pre>
