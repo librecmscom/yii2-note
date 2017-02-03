acbe79c393ae723462274a2a5b00bb4342d1968e
xuchao <85825770@qq.com>
2017-02-03 14:40:51 +0800

    add

--- src/views/note/view.php
+++ src/views/note/view.php
@@ -1,16 +1,13 @@
 <?php
 /* @var $this yii\web\View */
 use yii\helpers\Html;
-use xutl\highlightjs\HighlightJsAsset;
-
-HighlightJsAsset::register($this);
-$this->registerJs('
-    $(\'pre code\').each(function(i, block) {
-        hljs.highlightBlock(block);
-    });
-');
-
-
+use xutl\highlightjs\HighlightJs;
 ?>
 <?=$model->title;?>
-<pre class="<?=$model->format?>"><?=Html::encode($model->content);?></pre>
+
+
+<?php HighlightJs::begin([
+    'format'=>$model->format
+]);?>
+<?= Html::encode($model->content); ?>
+<?php HighlightJs::end()?>
